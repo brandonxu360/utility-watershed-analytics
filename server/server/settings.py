61e862e4ffe3e777b714c12fd37f3c09bc22cf48
brandonxu360 <brandon.xu2024@gmail.com>
2024-12-19 16:30:58 -0800

    Set up containers for PostgreSQL database, pgAdmin, and Django server, and configure Django server to use PostgreSQL PostGIS database

--- server/server/settings.py
+++ server/server/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis', # Use GIS
+
 ]
 
 MIDDLEWARE = [
@@ -74,9 +77,14 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # Use PostGIS database
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': os.environ.get("POSTGRES_DB"),
+        'USER': os.environ.get("POSTGRES_USER"),
+        'PASSWORD': os.environ.get("POSTGRES_PW"),
+        'HOST': "db",
+        'PORT': "5432"
     }
 }
 
