e02950e1014a4684d21aab248390b3c49a4d2882
brandonxu360 <brandon.xu2024@gmail.com>
2024-12-24 22:51:56 -0800

    Register WatershedBorder model with specialized GISModelAdmin class to allow admin management

--- server/server/watershed/admin.py
+++ server/server/watershed/admin.py
@@ -1,3 +1,4 @@
-from django.contrib import admin
+from django.contrib.gis import admin
+from .models import WatershedBorder
 
-# Register your models here.
+admin.site.register(WatershedBorder, admin.GISModelAdmin)
