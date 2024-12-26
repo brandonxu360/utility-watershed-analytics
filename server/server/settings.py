617b007c46cc3660ea57a9a55f4d3e09b0a285b5
brandonxu360 <brandon.xu2024@gmail.com>
2024-12-25 22:12:12 -0800

    Configure CORS to allow vite dev server

--- server/server/settings.py
+++ server/server/settings.py
@@ -41,10 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.gis',
     'rest_framework',
     'rest_framework_gis',
-    'server.watershed'
+    'server.watershed',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,7 +123,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+    
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
@@ -131,3 +133,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173', # Vite development server
+    'http://127.0.0.1:5173',
+]
