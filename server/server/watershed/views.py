379b50cbb01a4fcb8a7d5cdf2a1bff1f367a82ea
brandonxu360 <brandon.xu2024@gmail.com>
2025-02-03 01:45:35 -0800

    Update serializers and views for WatershedBorder model with improved descriptions and schema documentation

--- server/server/watershed/views.py
+++ server/server/watershed/views.py
@@ -1,23 +1,42 @@
 from rest_framework import viewsets
 from .models import WatershedBorder
 from .serializers import WatershedBorderSerializer, WatershedBorderBasicSerializer
+from drf_spectacular.utils import extend_schema
 
+@extend_schema(
+    description="""
+**Retrieve comprehensive watershed border data.**  
+
+- Provides **full details** for each watershed border  
+- Best used when selecting an **individual watershed**  
+
+Due to the detailed nature of this API, it is **not recommended** for retrieving large datasets at once.
+    """
+)
 class WatershedBorderViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    This ViewSet provides read-only access to WatershedBorder model instances,
-    allowing only GET requests to list all watershed borders or retrieve specific ones.
-    Attributes:
-        queryset: QuerySet of all WatershedBorder objects
-        serializer_class: Serializer class to convert WatershedBorder objects to/from JSON
+    This ViewSet provides read-only access to WatershedBorder model instances.
+
+    Note that the payload served by this view can be large and thus, requesting multiple WatershedBorder
+    model instances is not advised.
     """
     queryset = WatershedBorder.objects.all()
     serializer_class = WatershedBorderSerializer
 
+@extend_schema(
+    description="""
+**Retrieve simplified watershed border data.**  
+
+- Provides **only essential** information for each watershed border  
+- Best used when **retrieving multiple watershed borders** for visualization
+    """
+)
 class WatershedBorderBasicViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    This ViewSet provides a simplified read-only view of WatershedBorder model instances,
-    returning only basic fields. Like its parent class, it allows only GET requests to 
-    list all watershed borders or retrieve specific ones.
+    This ViewSet provides read-only access to simplified BasicWatershedBorder model instances.
+    
+    This endpoint is intended for scenarios where an overview of multiple watershed borders is required, 
+    particularly for mapping purposes where detailed geometry isn't necessary.
     """
     queryset = WatershedBorder.objects.all()
     serializer_class = WatershedBorderBasicSerializer
