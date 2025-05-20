555316bf761e263e699acc1a3197164c23baa0b6
brandonxu360 <brandon.xu2024@gmail.com>
2025-05-20 01:21:15 -0700

    Update current watershed border loaders to accomodate new data

--- server/server/watershed/load.py
+++ server/server/watershed/load.py
@@ -1,14 +1,14 @@
 from django.db import connection
-from .loaders.load_or import load_oregon_data
-from .loaders.load_wa import load_washington_data
+from .loaders.load_or_borders import load_oregon_borders
+from .loaders.load_wa_borders import load_washington_borders
 
 # Save the models to the database using the mapping
 def run(verbose=True):
     # Load the Oregon watersheds
-    load_oregon_data()
+    load_oregon_borders()
 
     # Load the Washington watersheds
-    load_washington_data()
+    load_washington_borders()
 
     # Update the simplified_geom field using PostGIS simplify (potentially more efficient than using geos simplify in the application)
     with connection.cursor() as cursor:
