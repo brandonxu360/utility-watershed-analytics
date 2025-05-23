fab82752b78cb697addcee821f59e2e600baf367
brandonxu360 <brandon.xu2024@gmail.com>
2025-05-23 11:05:45 -0700

    Refactor WatershedBorder model to use webcloud_run_id as primary key and update loading functions accordingly

--- server/server/watershed/loaders/load_wa_borders.py
+++ server/server/watershed/loaders/load_wa_borders.py
@@ -21,12 +21,12 @@ def load_washington_borders(verbose=True):
     lm = LayerMapping(WatershedBorder, wa_data_location, wa_mapping, transform=False)
     
     # Save records and get their IDs
-    existing_ids = set(WatershedBorder.objects.values_list('id', flat=True))  # IDs before insert
+    existing_ids = set(WatershedBorder.objects.values_list('webcloud_run_id', flat=True))  # IDs before insert
     lm.save(strict=True, verbose=verbose)
-    new_ids = set(WatershedBorder.objects.values_list('id', flat=True)) - existing_ids  # Newly inserted WA IDs
+    new_ids = set(WatershedBorder.objects.values_list('webcloud_run_id', flat=True)) - existing_ids  # Newly inserted WA IDs
 
     # Update only the newly inserted records with state='WA'
-    wa_borders = WatershedBorder.objects.filter(id__in=new_ids)
+    wa_borders = WatershedBorder.objects.filter(webcloud_run_id__in=new_ids)
     wa_borders.update(state='WA')
 
     print(f'Ingested WA Borders count: {wa_borders.count()}')
