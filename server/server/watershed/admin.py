1b658689216a938940626282f0368fad3ab523dd
brandonxu360 <brandon.xu2024@gmail.com>
2025-05-24 01:00:15 -0700

    Register Subcatchment and Channel models in the Django admin with GISModelAdmin

--- server/server/watershed/admin.py
+++ server/server/watershed/admin.py
@@ -1,4 +1,6 @@
 from django.contrib.gis import admin
-from .models import WatershedBorder
+from .models import WatershedBorder, Subcatchment, Channel
 
 admin.site.register(WatershedBorder, admin.GISModelAdmin)
+admin.site.register(Subcatchment, admin.GISModelAdmin)
+admin.site.register(Channel, admin.GISModelAdmin)
