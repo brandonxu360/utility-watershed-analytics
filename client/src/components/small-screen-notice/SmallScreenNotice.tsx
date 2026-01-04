f8f1af60c81a046d6c7069e4f872e6401c9abe8c
jakevanhalder <124746901+jakevanhalder@users.noreply.github.com>
2026-01-04 16:34:05 +0000

    Add aria alert role to small screen notice.

--- client/src/components/small-screen-notice/SmallScreenNotice.tsx
+++ client/src/components/small-screen-notice/SmallScreenNotice.tsx
@@ -3,7 +3,7 @@ import './SmallScreenNotice.css';
 export default function SmallScreenNotice(): JSX.Element {
     return (
         <div className="small-screen-notice">
-            <div>
+            <div role='alert'>
                 <h1 id="small-screen-title" className="small-screen-title">Best viewed on larger screens</h1>
                 <p className="small-screen-body">
                     This experience is optimized for tablets and desktops. For the clearest maps, charts, and controls, please use a device with a wider display.
