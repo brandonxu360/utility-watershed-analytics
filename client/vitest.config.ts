0f57eb7dcc7dc0e16a8fc3d110fc3f4daf17ba61
jakevanhalder <jakevanhalder@gmail.com>
2026-01-20 17:08:42 +0000

    Exclude route tree gen file from linter and vitest.

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, coverageConfigDefaults } from 'vitest/config';
 
 export default defineConfig({
     test: {
         environment: 'jsdom',
         setupFiles: './src/setupTests.ts',
         globals: true,
+        coverage: {
+            exclude: [
+                ...coverageConfigDefaults.exclude,
+                'src/routeTree.gen.ts',
+            ],
+        },
     },
 });
