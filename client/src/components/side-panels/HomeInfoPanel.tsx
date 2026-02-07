11d7fa976fcc5c2d2123d0030de3d2b539d74614
jakevanhalder <jakevanhalder@gmail.com>
2026-02-07 04:40:33 +0000

    Fix home page spacing.

--- client/src/components/side-panels/HomeInfoPanel.tsx
+++ client/src/components/side-panels/HomeInfoPanel.tsx
@@ -1,27 +1,39 @@
-/**
- * HomeSidePanelContent component that provides an overview of how to use the website.
- * Displays information about different watershed tiers and their significance.
- * 
- * @returns {JSX.Element} A sidebar panel containing watershed information.
- */
+import { Typography } from "@mui/material"
+import { tss } from "../../utils/tss";
+
+const useStyles = tss.create(({ theme }) => ({
+  root: {
+    padding: `${theme.spacing(2)} ${theme.spacing(0.25)}`,
+    display: 'flex',
+    flexDirection: 'column',
+    gap: theme.spacing(1.5),
+  },
+  heading: {
+    fontSize: theme.typography.h2.fontSize,
+  }
+}));
+
 export default function HomeSidePanelContent(): JSX.Element {
+  const { classes } = useStyles();
   return (
-    <div data-testid="home-panel">
-      <h2>Explore Watershed Analytics</h2>
-      <p>
+    <div className={classes.root} data-testid="home-panel">
+      <Typography variant="h2" className={classes.heading}><strong>Explore Watershed Analytics</strong></Typography>
+      <Typography variant="body1">
         Visualize and analyze hydrologic and environmental data for watersheds across the western United States.
         Gain insights into observed and modeled data to understand water and environmental conditions.
-      </p>
-      <h3>Tier 1 Watersheds</h3>
-      <p>
+      </Typography>
+      <Typography variant="h3"><strong>Tier 1 Watersheds</strong></Typography>
+      <Typography variant="body1">
         Access modeled results that provide initial insights but have not yet been calibrated.
-      </p>
-      <h3>Tier 2 Watersheds</h3>
-      <p>
+      </Typography>
+      <Typography variant="h3"><strong>Tier 2 Watersheds</strong></Typography>
+      <Typography variant="body1">
         Explore calibrated model results for enhanced accuracy and reliability.
         Start analyzing now to uncover trends, compare models, and support data-driven decisions for watershed management.
-      </p>
-      <strong>Get Started: Select a watershed to explore its data.</strong>
+      </Typography>
+      <Typography variant="body1">
+        <strong>Get Started: Select a watershed to explore its data.</strong>
+      </Typography>
     </div>
   );
 }
--- client/src/utils/theme.ts
+++ client/src/utils/theme.ts
@@ -74,7 +74,7 @@ const theme = createTheme({
         fontWeightRegular: 500,
         // Font size scale using rem
         h1: { fontSize: "3rem" },
-        h2: { fontSize: "2.25rem" },
+        h2: { fontSize: "2rem" },
         h3: { fontSize: "1.5rem" },
         h4: { fontSize: "1.25rem" },
         h5: { fontSize: "1.125rem" },
