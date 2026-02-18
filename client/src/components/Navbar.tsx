c25ec42945665218b931496ca75e03b6b6da5b8d
jakevanhalder <jakevanhalder@gmail.com>
2026-02-18 00:00:30 +0000

    Tweak color toggle spacing.

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -32,7 +32,7 @@ const useStyles = tss.create(({ theme }) => ({
   },
   colorToggle: {
     color: theme.palette.text.primary,
-    marginLeft: theme.spacing(2),
+    marginLeft: theme.spacing(1.75),
   },
 }));
 
